e20b514d15dbbc9ce1c28973b8356a6a4bb6c715
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-12-26 16:42:04 +0000

    espera passiva

--- src/Cliente/ClientConnection.java
+++ src/Cliente/ClientConnection.java
@@ -24,7 +24,13 @@ public class  ClientConnection {
     private BufferedReader sin;
     private Map<String,String> opts = new HashMap<>();
     private String lastCmd;
-    private volatile Boolean requestingInput;
+
+    private Boolean requestingInput;
+
+    private ReentrantLock lock;
+    private Condition con;
+
+
 
     public ClientConnection(Client c, Socket s) throws IOException {
         this.c = c;
@@ -33,40 +39,45 @@ public class  ClientConnection {
         this.br = new BufferedReader(new InputStreamReader(System.in));
         this.sin = new BufferedReader(new InputStreamReader(s.getInputStream()));
         this.requestingInput = false;
+        lock = new ReentrantLock();
+        con = lock.newCondition();
 
         Thread readTerminal = new Thread() {
             public void run() {
-                String current;
-                try {
-                    while ((current = br.readLine()) != null) {
-                        synchronized (requestingInput){
-                            if(requestingInput){
-                                String toAppend = opts.get(lastCmd);
-                                toAppend += " " + current;
-                                opts.replace(lastCmd, toAppend);
-                                requestingInput = false;
-                                requestingInput.notify();
+                    String current;
+                    try {
+                        while ((current = br.readLine()) != null) {
+                            if (requestingInput) {
+                                try {
+                                    lock.lock();
+                                    String toAppend = opts.get(lastCmd);
+                                    toAppend += " " + current;
+                                    opts.replace(lastCmd, toAppend);
+                                    requestingInput = false;
+                                    con.signal();
+                                }finally {
+                                    lock.unlock();
+                                }
                                 //System.out.println("Full command: " + toAppend);
-                            } else
-                            if(opts.containsKey(current)){
+                            } else if (opts.containsKey(current)) {
                                 // System.out.println(current);
                                 lastCmd = current;
                                 pw.println(opts.get(current));
                             } else {
                                 System.out.println("Not a valid input, use <optionNumber>");
                             }
+
                         }
+                        br.close();
+                        sin.close();
+                        pw.close();
+                        s.close();
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    br.close();
-                    sin.close();
-                    pw.close();
-                    s.close();
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+                    }
         };readTerminal.start();
 
 
@@ -92,38 +103,44 @@ public class  ClientConnection {
                 }
             }
 
-            private void readInterpreter(String message) throws Exception{
-                if(message.length() == 0) return; //prevenção de erros
+            private void readInterpreter(String message) throws Exception {
+                try {
+                    lock.lock();
+                    if (message.length() == 0) return; //prevenção de erros
 
-                String output = "";
-                //System.out.println("Message: " +message);
-                if(message.charAt(0) == '$'){ // caso seja um comando que o utilizador possa usar
-                    String[] subS = message.substring(1).split(" ", 3);
-                    opts.put(Integer.toString(opts.size()+1), subS[0]); // guardar numero correspondente à opçã
-                    //System.out.println("Command :" + subS[0]);
-                    switch (subS[0]){
-                        case "Request":
-                            requestingInput = true;
-                            System.out.println(subS[2]);
-                            while(requestingInput)
-                                synchronized(requestingInput){
-                                        requestingInput.wait();
+                    String output = "";
+                    //System.out.println("Message: " +message);
+                    if (message.charAt(0) == '$') { // caso seja um comando que o utilizador possa usar
+                        String[] subS = message.substring(1).split(" ", 3);
+                        opts.put(Integer.toString(opts.size() + 1), subS[0]); // guardar numero correspondente à opçã
+                        //System.out.println("Command :" + subS[0]);
+                        switch (subS[0]) {
+                            case "Request":
+                                requestingInput = true;
+                                System.out.println(subS[2]);
+                                while (requestingInput) {
+                                    con.await();
                                 }
-                            break;
-                        case "SendReply":
-                            requestingInput = false;
-                            pw.println(opts.get(lastCmd));
-                            break;
-                        case "Clear":
-                            opts = new HashMap<>();
-                            break;
-                        default:
-                            output += opts.size() + " - ";
-                            output += subS[2];
-                            System.out.println(output);
-                            break;
-                    }
-                } else System.out.println(message);
+
+
+                                break;
+                            case "SendReply":
+                                requestingInput = false;
+                                pw.println(opts.get(lastCmd));
+                                break;
+                            case "Clear":
+                                opts = new HashMap<>();
+                                break;
+                            default:
+                                output += opts.size() + " - ";
+                                output += subS[2];
+                                System.out.println(output);
+                                break;
+                        }
+                    } else System.out.println(message);
+                }finally {
+                    lock.unlock();
+                }
             }
 
         };readServer.start();
